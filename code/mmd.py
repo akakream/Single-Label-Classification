b826ed8f33d67daceb845314539784317bb1b43c
ahmetkerem <akeremtuberlin@gmail.com>
2020-06-19 20:57:59 +0200

    mmd fixed

--- code/mmd.py
+++ code/mmd.py
@@ -1,35 +1,44 @@
 import tensorflow as tf
+from functools import reduce
 
-def gaussian_kernel(X,Y):
+def partial_mmd(X,Y):
     '''
-    Gaussian kernel
-    input: tensor with shape(None,)
+    Pairwise dsitance
+    uses gaussian kernel
+    input: tensor with shape(batch_size,a,b,c,...)
     output: scalar
     '''
 
-    #print(f'THIS IS X: {X}')
-    k = tf.math.subtract(X,Y)
-    #print(f'This is after subtract: {k}')
-    k = tf.cast(k, dtype=tf.float32)
-    #print(f'This is after cast: {k}')
-    k = tf.norm(k, axis=1)
-    #print(f'This is after norm: {k}')
-    k = tf.math.square(k)
-    #print(f'This is after square: {k}')
-    k = -k/2
-    #print(f'This is after -k/2: {k}')
-    k = tf.math.exp(k)
-    #print(f'This is after exp: {k}')
-    kernel = tf.math.reduce_sum(k)
-    #print(f'This is after reduce_sum: {kernel}')
+    # Allows pairwise subtraction by adding additional dimensions and broadcasting
+    X_ = tf.expand_dims(X,1)
+    Y_ = tf.expand_dims(Y,0)
+    k = tf.math.subtract(X_,Y_)
 
+    # tf.norm needs float32
+    k = tf.cast(k, dtype=tf.float32)
+
+    # To get the norms of the tensors that come from intermediate layers, the tensors are reshaped into vectors. Afterwards, the norms of the vectors are taken
+    dims = list(k.shape)
+    dims_to_reduce = dims[2:] # all dimension except the first two (they are batch_sizes) will be reshaped into vectors
+    reduced_dim = reduce(lambda x,y: x*y, dims_to_reduce)
+
+    reshaped_k = tf.reshape(k, dims[:2]+[reduced_dim]) # result: (batch_size, batch_size, N)
+
+    k = tf.norm(reshaped_k, axis=2) # result: (batch_size, batch_size)
+    
+    k = tf.math.square(k)
+    k = -k/2.0
+    k = tf.math.exp(k)
+    
+    kernel = tf.math.reduce_mean(k)
+    
     return kernel
 
 def mmd_helper(X,Y):
     
-    kernel = gaussian_kernel(X,Y)
+    kernel = partial_mmd(X,Y)
 
-    return kernel / (len(X) * len(Y))
+    return kernel
 
 def mmd(X,Y):
     '''
@@ -54,8 +63,8 @@ def test_mmd():
     c = tf.constant([0,1,2,3,4])
     d = tf.constant([5,6,7,8,9])
 
-    e = tf.constant([[[0,34,2,3,4],[5,34,76,82,9],[32,342,532,23,1]], [[3,23,432,342,43],[51,2,72,4,9],[23,3,5323,22,32]]])
-    f = tf.constant([[[5,2,7,238,9],[0,1,25,33,4],[32,54,15,78,4]], [[6,345,56,76,78],[556,4,766,2,95],[2,32,578,268,657]]])
+    e = tf.constant([[[[0,34,2,3,4],[5,34,76,82,9],[32,342,532,23,1]], [[3,23,432,342,43],[51,2,72,4,9],[23,3,5323,22,32]]]])
+    f = tf.constant([[[[5,2,7,238,9],[0,1,25,33,4],[32,54,15,78,4]], [[6,345,56,76,78],[556,4,766,2,95],[2,32,578,268,657]]]])
 
     t = mmd(e,f)
     print(t)
